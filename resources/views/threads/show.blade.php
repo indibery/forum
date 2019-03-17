66658674b420148a3ad843761671c03d49758c2a
Kim KwangSu <bery@indischool.com>
2019-03-17 13:34:54 +0900

    forum 3강 A Thread Can Have Replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -4,13 +4,20 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card mb-3">
                 <div class="card-header">{{ $thread->title }}</div>
-
                 <div class="card-body">
                     {{ $thread->body }}
                 </div>
             </div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @foreach($thread->replies as $reply)
+                @include('threads.reply')
+            @endforeach
         </div>
     </div>
 </div>
