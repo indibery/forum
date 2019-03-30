6c8bf6caa8d4efcfcfba89c752d914b8a6751b70
Kim KwangSu <bery@indischool.com>
2019-03-31 02:40:46 +0900

    6강 CreateThreadTest Add

---
+++ tests/Feature/CreateThreadTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateThreadTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function an_authenticated_user_can_create_new_forum_thread()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->actingAs(factory('App\User')->create());
+
+        $thread = factory('App\Thread')->make();
+
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())->assertSee($thread->title)->assertSee($thread->body);
+    }
+
+    /** @test */
+    public function guests_may_not_create_threads()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $thread = factory('App\Thread')->make();
+
+        $this->post('/threads', $thread->toArray());
+    }
+}
