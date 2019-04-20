fc1448c7c3a14e3ebe1266b33d6fb39d01b7ac16
Kim KwangSu <bery@indischool.com>
2019-04-20 20:14:38 +0900

    10강 완료 routes

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/threads', 'ThreadController@index');
-Route::get('/threads/create', 'ThreadController@create');
-Route::get('/threads/{channel}/{thread}', 'ThreadController@show');
-Route::post('/threads', 'ThreadController@store');
-// index, create, store, show 등이 모두 갖추어져 있는 경우에는 라우트 파일에 resource로 등록
-// Route::resource('threads', 'ThreadController');
+Route::get('threads', 'ThreadController@index');
+Route::get('threads/create', 'ThreadController@create');
+Route::get('threads/{channel}/{thread}', 'ThreadController@show');
+Route::post('threads', 'ThreadController@store');
+
 
 Route::post('/threads/{channel}/{thread}/replies', 'ReplyController@store');
 
