4d85d77b7f6bd250d233d7485da1c28c1368b577
Kim KwangSu <bery@indischool.com>
2019-04-24 14:09:58 +0900

    16강 Meta Details and Pagination 완료

--- app/Thread.php
+++ app/Thread.php
@@ -8,6 +8,17 @@ class Thread extends Model
 {
     protected $guarded = [];
 
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('replyCount', function ($builder) {
+            $builder->withCount('replies');
+        });
+
+    }
+
     public function path()
     {
         return "/threads/{$this->channel->slug}/$this->id";
@@ -16,6 +27,11 @@ class Thread extends Model
     public function replies()
     {
         return $this->hasMany(Reply::class);
+    }
+
+    public function getReplyCountAttribute()
+    {
+        return $this->replies()->count();
     }
 
     public function creator()
