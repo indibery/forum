5f1c6cf9aba148133b1d1d1d66a8fb58602ced12
Kim KwangSu <bery@indischool.com>
2019-05-14 01:01:55 +0900

    25강 25. How to Construct an Activity Feed with TDD
    이번강의는 어려웠다 특히 구조가 달라져서 데이터를 지우고 다시 했는데 팩토리로 살리는 법도 몰르고 어찌어찌 복구는 되었다. 막판엔 졸려서 겨우했네... 이번강좌는 나의 히스토리인데 다양한 히스토리를 만드는 방법을 알아둬야겠다.

--- app/Reply.php
+++ app/Reply.php
@@ -6,14 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    use Favoritable;
+    use Favoritable, RecordsActivity;
     protected $guarded = [];
 
     protected $with = ['owner', 'favorites'];
 
     /**
      * A reply has an owner.
-     * 
+     *
      * @regurn \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function owner()
